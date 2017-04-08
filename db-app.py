c1f40f7c6828ba3f93d87191a59f90cfb2fa9527
Claudia Yan <claudiab.yan@gmail.com>
2017-04-08 12:04:24 -0400

    started up the index, one way , round trip forms and nav navigation

--- db-app.py
+++ db-app.py
@@ -6,5 +6,14 @@ def main():
     return render_template('index.html')
 
 
+@app.route('/one-way')
+def one_way():
+    return render_template('one-way.html')
+
+@app.route('/round-trip')
+def round_trip():
+    return render_template('round-trip.html')
+
+
 if __name__ == "__main__":
     app.run()
