0a9cc59d8d328db2022b134fbdcb2be743591f1b
Claudia Yan <claudiab.yan@gmail.com>
2017-04-11 00:30:56 -0400

    added submit buttons and rerouted the topnav

--- db-app.py
+++ db-app.py
@@ -1,19 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route("/")
+@app.route('/<path>')
 def main():
     return render_template('index.html')
 
 
 @app.route('/one-way')
 def one_way():
+    # _dep_date = request.form['departure-date']
+    # _dep_time = request.form['departure-time']
+    # _outgoing_station = request.form['from-station']
+    # _destination_station = request.form['to-station']
     return render_template('one-way.html')
 
 
 @app.route('/round-trip')
 def round_trip():
     return render_template('round-trip.html')
+
+
+
 
 
 if __name__ == "__main__":
