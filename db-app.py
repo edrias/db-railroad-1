4ba5d26d87952d1a6b5a537d35caa4e8f0281237
Claudia Yan <claudiab.yan@gmail.com>
2017-04-20 12:23:17 -0400

    round trip form submits data - is accessible

--- db-app.py
+++ db-app.py
@@ -11,21 +11,10 @@ def main():
 #@app.route('/one-way/<path>',methods=["GET","POST"])
 @app.route('/one-way/')
 def one_way():
-    # form = OneWayForm()
-    # if form.validate_on_submit():
-    #     _dep_date = request.form['departure-date']
-    #     _dep_time = request.form['departure-time']
-    #     _outgoing_station = request.form['from-station']
-    #     _destination_station = request.form['to-station']
-    #     print(_dep_date)
-    #     print (_dep_time)
-    #     print (_outgoing_station)
-    #     print (_destination_station)
-    #     return "hello?"
     return render_template('one-way.html')
 
 @app.route('/oneway-action',methods=["POST"])
-def one_way_sub():
+def one_way_act():
     if request.method == "POST":
         _dep_date = request.form['departure-date']
         _dep_time = request.form['departure-time']
@@ -35,13 +24,33 @@ def one_way_sub():
         print (_dep_time)
         print (_outgoing_station)
         print (_destination_station)
-    return "wtf"
+        # PROCESS VALUES HERE FOR QUERIES
+    return "result template"
 
-    # return  render_template('index.html')
 
 @app.route('/round-trip/')
 def round_trip():
     return render_template('round-trip.html')
+
+
+@app.route('/round-trip-action', methods =['POST'])
+def round_trip_act():
+    if request.method =='POST':
+        _dep_date = request.form['departure-date']
+        _dep_time = request.form['departure-time']
+        _return_date = request.form['return-date']
+        _return_time = request.form['return-time']
+
+        _outgoing_station = request.form['from-station']
+        _destination_station = request.form['to-station']
+        print(_dep_date)
+        print (_dep_time)
+        print(_return_date)
+        print (_return_time)
+        print (_outgoing_station)
+        print (_destination_station)
+        # PROCESS VALUES HERE
+        return "result template"
 
 
 if __name__ == "__main__":
