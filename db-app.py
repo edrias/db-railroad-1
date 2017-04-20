5a0bc9292a6e2f2524f6439ab013dff58ea18471
Claudia Yan <claudiab.yan@gmail.com>
2017-04-20 12:04:44 -0400

    no longer using wtf forms

--- db-app.py
+++ db-app.py
@@ -9,10 +9,24 @@ def main():
 
 
 #@app.route('/one-way/<path>',methods=["GET","POST"])
-@app.route('/one-way/',methods=["GET","POST"])
+@app.route('/one-way/')
 def one_way():
-    form = OneWayForm()
-    if form.validate_on_submit():
+    # form = OneWayForm()
+    # if form.validate_on_submit():
+    #     _dep_date = request.form['departure-date']
+    #     _dep_time = request.form['departure-time']
+    #     _outgoing_station = request.form['from-station']
+    #     _destination_station = request.form['to-station']
+    #     print(_dep_date)
+    #     print (_dep_time)
+    #     print (_outgoing_station)
+    #     print (_destination_station)
+    #     return "hello?"
+    return render_template('one-way.html')
+
+@app.route('/oneway-action',methods=["POST"])
+def one_way_sub():
+    if request.method == "POST":
         _dep_date = request.form['departure-date']
         _dep_time = request.form['departure-time']
         _outgoing_station = request.form['from-station']
@@ -21,11 +35,7 @@ def one_way():
         print (_dep_time)
         print (_outgoing_station)
         print (_destination_station)
-        return "hello?"
-    return render_template('one-way.html', form = form)
-
-# @app.route('/oneway-action',methods=["GET","POST"])
-# def one_way_sub():
+    return "wtf"
 
     # return  render_template('index.html')
 
