c6186982501a3b7a44f7cb2a9662874ef470924f
Claudia Yan <claudiab.yan@gmail.com>
2017-05-13 00:34:10 -0400

    removed google stuff - added in eddies content

---
+++ models/db-queries.py
@@ -0,0 +1,28 @@
+import sqlite3 as sql
+
+#Use this class to access DB
+
+class db_connect():
+
+    def __int__(self):
+        with sql.connect("db-railroad.db") as self.con:
+            self.cur = self.con.cursor()
+
+
+    def get_one_way_trip(self, dep_date,dep_time,outgoing_station,destination_station):
+        #TODO: write query
+        return None
+
+    def get_round_trip(self,dep_date,dep_time,return_date,return_time, outgoing_station,destination_station):
+        #TODO: write query
+        return None
+
+    def insert_purchase(self,trip_details,fname,lname,email,billing_addr,credit_card):
+        #TODO: write query
+        return None
+
+
+
+
+
+
